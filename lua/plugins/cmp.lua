72e5d0c701629bc320233f6215e32c54fa4fdf32
Leo-Clerc <Leo.Clerc@ekimetrics.com>
2023-07-25 14:12:51 +0200

    Restructure plugin configuration, setup cmp and autopairs

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-path",
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    event = "InsertEnter",
+  },
+  config = function()
+
+  end
+}
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,9 +1,17 @@
 local options = {
   backup = false,
+  swapfile = false,
   mouse = "a",
+  smartindent = true,
+  showtabline = 0,
+  expandtab = true,
+  tabstop = 2,
+  shiftwidth = 2,
   number = true,
   relativenumber = true,
   termguicolors = true,
+  numberwidth = 4,
+  wrap = false,
 }
 
 for k, v in pairs(options) do
