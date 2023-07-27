15623ca22f7bd32ce9d388ed3a388f0dfffda1ce
Leo-Clerc <Leo.Clerc@ekimetrics.com>
2023-07-27 15:01:47 +0200

    Add config for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -12,6 +12,8 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip",
     },
+    config = require "plugins.config.cmp"
   },
 }
