0e7c58b284206eb28abb8039f6e3dbae4c74a746
Leo-Clerc <leocoursadistance@gmail.com>
2023-08-08 23:16:45 +0200

    Add telescope, and a command to open it

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    "nvim-telescope/telescope-fzf-native.nvim",
+  },
+}
