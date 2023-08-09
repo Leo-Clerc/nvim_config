46d4ec36e9b6d6b2213682a53af17b9969e66eb1
Leo-Clerc <leocoursadistance@gmail.com>
2023-08-09 23:16:24 +0200

    Add scroll sount. Trying to implement lsp

--- lua/plugins/config/heirline.lua
+++ lua/plugins/config/heirline.lua
@@ -58,28 +58,28 @@ return function()
         ["!"] = "!",
         t = "T",
       },
-      mode_colors = {
-        n = "red" ,
-        i = "green",
-        v = "cyan",
-        V =  "cyan",
-        ["\22"] =  "cyan",
-        c =  "orange",
-        s =  "purple",
-        S =  "purple",
-        ["\19"] =  "purple",
-        R =  "orange",
-        r =  "orange",
-        ["!"] =  "red",
-        t =  "red",
-      }
+      -- mode_colors = {
+      --   n = "red" ,
+      --   i = "green",
+      --   v = "cyan",
+      --   V =  "cyan",
+      --   ["\22"] =  "cyan",
+      --   c =  "orange",
+      --   s =  "purple",
+      --   S =  "purple",
+      --   ["\19"] =  "purple",
+      --   R =  "orange",
+      --   r =  "orange",
+      --   ["!"] =  "red",
+      --   t =  "red",
+      -- }
     },
     provider = function(self)
       return "%2("..self.mode_names[self.mode].."%)"
     end,
     hl = function(self)
       local mode = self.mode:sub(1, 1) 
-      return { fg = self.mode_colors[mode], bold = true, }
+      return { fg = "gray", bold = true, } -- self.mode_colors[mode] for colors in comment before
     end,
     update = {
       "ModeChanged",
@@ -96,19 +96,20 @@ return function()
       end,
   }
 
-  local FileIcon = {
-      init = function(self)
-          local filename = self.filename
-          local extension = vim.fn.fnamemodify(filename, ":e")
-          self.icon, self.icon_color = require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
-      end,
-      provider = function(self)
-          return self.icon and (self.icon .. " ")
-      end,
-      hl = function(self)
-          return { fg = self.icon_color }
-      end
-  }
+  -- Table for file icon in status line. I don't like it, might removve this code later.
+  -- local FileIcon = {
+  --     init = function(self)
+  --         local filename = self.filename
+  --         local extension = vim.fn.fnamemodify(filename, ":e")
+  --         self.icon, self.icon_color = require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+  --     end,
+  --     provider = function(self)
+  --         return self.icon and (self.icon .. " ")
+  --     end,
+  --     hl = function(self)
+  --         return { fg = self.icon_color }
+  --     end
+  -- }
 
   local FileName = {
       provider = function(self)
@@ -124,7 +125,7 @@ return function()
           end
           return filename
       end,
-      hl = { fg = utils.get_highlight("Directory").fg },
+      hl = { fg = "gray" }, -- utils.get_highlight("Directory").fg to get color of colorscheme
   }
 
   local FileFlags = {
@@ -133,14 +134,14 @@ return function()
               return vim.bo.modified
           end,
           provider = "[+]",
-          hl = { fg = "green" },
+          hl = { fg = "gray" },
       },
       {
           condition = function()
               return not vim.bo.modifiable or vim.bo.readonly
           end,
           provider = "",
-          hl = { fg = "orange" },
+          hl = { fg = "gray" },
       },
   }
 
@@ -153,19 +154,19 @@ return function()
       hl = function()
           if vim.bo.modified then
               -- use `force` because we need to override the child's hl foreground
-              return { fg = "cyan", bold = true, force=true }
+              return { fg = "gray", bold = true, force=true }
           end
       end,
   }
 
   -- let's add the children to our FileNameBlock component
   FileNameBlock = utils.insert(FileNameBlock,
-      FileIcon,
+      -- FileIcon,
       utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
       FileFlags,
       { provider = '%<'} -- this means that the statusline is cut here when there's not enough space
   )
-  
+
   local FileType = {
     provider = function()
       return string.upper(vim.bo.filetype)
@@ -177,11 +178,64 @@ return function()
     provider = "%7(%l/%3L%):%2c %P",
   }
 
+  local Diagnostics = {
+
+      condition = conditions.has_diagnostics,
+
+      static = {
+          -- error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+          -- warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+          -- info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+          -- hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+      },
+
+      init = function(self)
+          self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+          self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+          self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+          self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+      end,
+
+      update = { "DiagnosticChanged", "BufEnter" },
+
+      {
+          provider = "![",
+      },
+      {
+          provider = function(self)
+              -- 0 is just another output, we can decide to print it or not!
+              return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+          end,
+          hl = { fg = "gray" },
+      },
+      {
+          provider = function(self)
+              return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+          end,
+          hl = { fg = "gray" },
+      },
+      {
+          provider = function(self)
+              return self.info > 0 and (self.info_icon .. self.info .. " ")
+          end,
+          hl = { fg = "gray" },
+      },
+      {
+          provider = function(self)
+              return self.hints > 0 and (self.hint_icon .. self.hints)
+          end,
+          hl = { fg = "diag_hint" },
+      },
+      {
+          provider = "]",
+      },
+  }
   local statusline = {
-    ViMode,
     FileNameBlock,
+    ViMode,
     -- FileType,
     Ruler,
+    Diagnostics,
   }
 
   local opts = {
