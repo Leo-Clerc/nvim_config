c5f3bbeb20db8dd8604bc32bc73b52a3cba34e4a
Leo-Clerc <leocoursadistance@gmail.com>
2023-08-16 17:20:17 +0200

    Changes some icons for alpha

--- lua/plugins/config/alpha.lua
+++ lua/plugins/config/alpha.lua
@@ -34,11 +34,11 @@ return function()
  }
   -- TODO: Add other buttons. One with telescope, I guess ?
   local buttons = {
-    dashboard.button( "e", " New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f f", "󰱼 Search file" , ":lua require('telescope.builtin').find_files()<CR>"),
-    dashboard.button( "f w", "󰈬 Search word", ":Telescope live_grep<CR>"),
-    dashboard.button( "h", " Harpoon menu", ':lua require("harpoon.ui").toggle_quick_menu()<CR>'),
-    dashboard.button( "q", "󰩈 Quit", ":qa<CR>"),
+    dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f f", "  Search file" , ":lua require('telescope.builtin').find_files()<CR>"),
+    dashboard.button( "f w", "󰈭  Search word", ":Telescope live_grep<CR>"),
+    dashboard.button( "h", "   Harpoon menu", ':lua require("harpoon.ui").toggle_quick_menu()<CR>'),
+    dashboard.button( "q", "󰩈  Quit", ":qa<CR>"),
   }
   local function footer()
     --local total_plugins =
