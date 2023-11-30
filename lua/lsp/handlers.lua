adea19ab0df391d054e5017d675bb7774f47eba6
Leo-Clerc <leocoursadistance@gmail.com>
2023-11-30 16:25:46 +0100

    Correctly define mappings for all lsp

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -3,17 +3,16 @@ return {
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     local on_attach = require "lsp.on_attach"
     local opts = {
-      settings = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      },
+      capabilities = capabilities,
+      on_attach = on_attach,
     }
-    require("lspconfig")[server_name].setup {opts}
+    require("lspconfig")[server_name].setup(opts)
 
   end,
  ["lua_ls"] = function ()
+    local on_attach = require "lsp.on_attach"
      require("lspconfig").lua_ls.setup {
-         on_attach = require "lsp.on_attach",
+         on_attach = on_attach,
          capabilities = require("cmp_nvim_lsp").default_capabilities(),
          settings = {
              Lua = {
